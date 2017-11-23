b0775f21537268ec5c3162fc36755b49ad679e96
crisyantoparulian <crisyanto.p@gmail.com>
2017-11-23 17:17:50 +0700

    API Auth

---
+++ app/Http/Controllers/AuthenticateController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\http\Requests;
+use JWTAuth;
+use App\User;
+use Hash;
+
+class AuthenticateController extends Controller
+{
+    public function __construct(User $user)
+    {
+    	$this->user = $user;
+    }
+
+    public function login(Request $request)
+    {
+    	$credential = $request->only(['email','password']);
+
+    	if(!$token = JWTAuth::attempt($credential)){
+    		return response()->json(['error', 'Invalid credential'], 401);
+    	}
+    	return response()->json(compact('token'));
+    }
+
+    public function register(Request $request)
+    {
+    	$credential = $request->only(['name','email','password']);
+    	$credential = [
+    		'name'=>$credential['name'],
+    		'email'=>$credential['email'],
+    		'password'=>Hash::make($credential['password'])
+    	];
+    	try{
+    		$user = $this->user->create($credential);
+    	}catch(Exception $e){
+    		return response()->json(['error'=>'User Already Exist'], 409);
+    	}
+    	$token = JWTAuth::fromUser($user);
+
+
+    	return response()->json(compact('token'));
+    }
+}
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\http\Requests;
+use App\User;
+class UserController extends Controller
+{
+    public function __construct(User $user)
+    {
+    	$this->user = $user;
+    }
+
+    public function index()
+    {
+    	return $this->user->all();
+    }
+
+    public function show($id)
+    {
+    	$data = $this->user->where('id', $id)->get();
+    	return response()->json(compact('data'),200);
+    }
+}
